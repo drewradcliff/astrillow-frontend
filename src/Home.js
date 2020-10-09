5a9c11558b57943c023c58acbb2bd6993bb15631
Drew Radcliff <sirampr@gmail.com>
2020-10-09 09:53:21 -0400

    Correct css filename

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import image from "./images/133303main_image_feature_406_ys_full.jpg";
-import "./Home.css";
 import AsteroidPage from "./components/AsteroidPage";
 
 export default function Home() {
--- src/Saved.js
+++ src/Saved.js
@@ -1,27 +1,18 @@
 import React from "react";
-import image from "./images/133303main_image_feature_406_ys_full.jpg";
 import SavedAsteroidCard from "./components/SavedAsteroidCard";
-import "./Home.css";
+import "./Saved.css";
 
 export default function Home() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        backgroundImage: `url(${image})`,
-        backgroundSize: "cover",
-      }}
-    >
-      <div>
-        <h1 className="title">Saved Asteroids</h1>
-        <div className="cards">
-          <SavedAsteroidCard className="card" />
-          <SavedAsteroidCard className="card" />
-          <SavedAsteroidCard className="card" />
-          <SavedAsteroidCard className="card" />
-          <SavedAsteroidCard className="card" />
-          <SavedAsteroidCard className="card" />
-        </div>
+    <div>
+      <h1 className="title">Saved Asteroids</h1>
+      <div className="cards">
+        <SavedAsteroidCard className="card" />
+        <SavedAsteroidCard className="card" />
+        <SavedAsteroidCard className="card" />
+        <SavedAsteroidCard className="card" />
+        <SavedAsteroidCard className="card" />
+        <SavedAsteroidCard className="card" />
       </div>
     </div>
   );
