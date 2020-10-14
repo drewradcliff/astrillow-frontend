44acadc3fb9597c1e53ad085cd7f88c1d45b6e6a
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-10-14 12:00:13 -0600

    Begin implementing space act/about page.

--- src/App.js
+++ src/App.js
@@ -6,135 +6,132 @@ import Saved from "./Saved";
 import Search from "./Search";
 import Signup from "./Signup";
 import Login from "./Login";
+import SpaceAct from "./SpaceAct";
 import { useHistory } from "react-router-dom";
 
-
 function App() {
-  const history = useHistory();
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    const data = localStorage.getItem("user");
-    if (data) {
-      setUser(JSON.parse(data));
-    }
-  }, []);
-  useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(user));
-  });
+	const history = useHistory();
+	const [user, setUser] = useState(null);
+	useEffect(() => {
+		const data = localStorage.getItem("user");
+		if (data) {
+			setUser(JSON.parse(data));
+		}
+	}, []);
+	useEffect(() => {
+		localStorage.setItem("user", JSON.stringify(user));
+	});
 
-  const [asteroidDetail, setAsteroidDetail] = useState([]);
-  const [status, setStatus] = useState("not logged in");
-  const [loggedIn, setLoggedIn] = useState(
-    localStorage.getItem("token") ? true : false
-  );
-  function handleLogin(e, data) {
-    e.preventDefault();
-    fetch("http://localhost:8000/auth/login/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        if (json.user) {
-          console.log("HIT");
-          handleSuccessfulAuth(json.user);
-        }
-        console.log(json);
-        localStorage.setItem("token", json.token);
-        // localStorage.setItem("user_id", json.user.id)
-        setLoggedIn(true);
-        setStatus("logged in");
-      })
-      .catch((error) => {
-        console.log("error", error);
-      });
-  }
+	const [asteroidDetail, setAsteroidDetail] = useState([]);
+	const [status, setStatus] = useState("not logged in");
+	const [loggedIn, setLoggedIn] = useState(
+		localStorage.getItem("token") ? true : false
+	);
+	function handleLogin(e, data) {
+		e.preventDefault();
+		fetch("http://localhost:8000/auth/login/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				if (json.user) {
+					console.log("HIT");
+					handleSuccessfulAuth(json.user);
+				}
+				console.log(json);
+				localStorage.setItem("token", json.token);
+				// localStorage.setItem("user_id", json.user.id)
+				setLoggedIn(true);
+				setStatus("logged in");
+			})
+			.catch((error) => {
+				console.log("error", error);
+			});
+	}
 
-  function handleSignup(e, data) {
-    e.preventDefault();
-    fetch("http://localhost:8000/users/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        localStorage.setItem("token", json.token);
-        setLoggedIn(true);
-        setUser(json.user);
-      });
-  }
+	function handleSignup(e, data) {
+		e.preventDefault();
+		fetch("http://localhost:8000/users/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				localStorage.setItem("token", json.token);
+				setLoggedIn(true);
+				setUser(json.user);
+			});
+	}
 
-  function handleLogout() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    setUser(null);
-    setLoggedIn(false);
-    setStatus("not logged in");
-  }
+	function handleLogout() {
+		localStorage.removeItem("token");
+		localStorage.removeItem("user");
+		setUser(null);
+		setLoggedIn(false);
+		setStatus("not logged in");
+	}
 
-  function handleSuccessfulAuth(data) {
-    setUser(data);
-    setStatus("logged in");
-    history.push("/");
-  }
+	function handleSuccessfulAuth(data) {
+		setUser(data);
+		setStatus("logged in");
+		history.push("/");
+	}
 
-  return (
-    <>
-      <Menu loggedIn={loggedIn} handleLogout={handleLogout} />
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={(props) => <Home {...props} status={status} user={user} />}
-        ></Route>
-        <Route path="/saved">
-          <Saved />
-        </Route>
-        <Route
-          path="/search"
-          render={(props) => (
-            <Search
-              {...props}
-              setAsteroidDetail={setAsteroidDetail}
-              user={user}
-            />
-          )}
-        ></Route>
-        <Route
-          path="/signup"
-          render={(props) => (
-            <Signup {...props} handleSignup={handleSignup} user={user} />
-          )}
-        ></Route>
-        <Route
-          path="/login"
-          render={(props) => (
-            <Login {...props} handleLogin={handleLogin} user={user} />
-          )}
-        ></Route>
-        <Route
-          path="/asteroid-detail"
-          render={(props) => (
-            <AsteroidPage
-              {...props}
-              asteroidDetail={asteroidDetail}
-              user={user}
-            />
-          )}
-        ></Route>
-        <Route
-          path="*"
-          render={(props) => <FourOhFour {...props} user={user} />}
-        ></Route>
-      </Switch>
-    </>
-  );
+	return (
+		<>
+			<Menu loggedIn={loggedIn} handleLogout={handleLogout} />
+			<Switch>
+				<Route
+					exact
+					path="/"
+					render={(props) => (
+						<Home {...props} status={status} user={user} />
+					)}></Route>
+				<Route path="/saved">
+					<Saved />
+				</Route>
+				<Route
+					path="/search"
+					render={(props) => (
+						<Search
+							{...props}
+							setAsteroidDetail={setAsteroidDetail}
+							user={user}
+						/>
+					)}></Route>
+				<Route exact path="/spaceact"></Route>
+				<Route
+					path="/signup"
+					render={(props) => (
+						<Signup {...props} handleSignup={handleSignup} user={user} />
+					)}></Route>
+				<Route
+					path="/login"
+					render={(props) => (
+						<Login {...props} handleLogin={handleLogin} user={user} />
+					)}></Route>
+				<Route
+					path="/asteroid-detail"
+					render={(props) => (
+						<AsteroidPage
+							{...props}
+							asteroidDetail={asteroidDetail}
+							user={user}
+						/>
+					)}></Route>
+				<Route
+					path="*"
+					render={(props) => <FourOhFour {...props} user={user} />}></Route>
+			</Switch>
+		</>
+	);
 }
 
 export default App;
---
+++ src/SpaceAct.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SpaceAct() {
+	return (
+		<div className="space act">
+			<h1 className="title">Space Act</h1>
+			<div className="cards">
+				<h3 className="card"> </h3>
+			</div>
+		</div>
+	);
+}
