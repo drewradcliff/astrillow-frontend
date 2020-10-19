18b8c9ea52af70737520b59496da787fb94d5bf6
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-10-19 07:50:33 -0600

    Updated CSS to add grid view back to Saved Asteroids.

--- src/Saved.js
+++ src/Saved.js
@@ -5,45 +5,45 @@ import { useHistory } from "react-router-dom";
 import "./Saved.css";
 
 export default function Saved(props) {
-  let history = useHistory();
-  const [savedAsteroids, setSavedAsteroids] = useState([]);
-  useEffect(() => {
-    fetch(
-      `http://localhost:8000/api/savedasteroid/?saved_by=${
-        JSON.parse(localStorage.getItem("user")).id
-      }`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `JWT ${localStorage.getItem("token")}`,
-        },
-      }
-    )
-      .then((res) => {
-        if (res.status === 401) {
-          history.push("/login");
-        }
-        return res.json();
-      })
-      .then((data) => setSavedAsteroids(data))
-      .catch((error) => console.error(error));
-  }, [history]);
+	let history = useHistory();
+	const [savedAsteroids, setSavedAsteroids] = useState([]);
+	useEffect(() => {
+		fetch(
+			`http://localhost:8000/api/savedasteroid/?saved_by=${
+				JSON.parse(localStorage.getItem("user")).id
+			}`,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `JWT ${localStorage.getItem("token")}`,
+				},
+			}
+		)
+			.then((res) => {
+				if (res.status === 401) {
+					history.push("/login");
+				}
+				return res.json();
+			})
+			.then((data) => setSavedAsteroids(data))
+			.catch((error) => console.error(error));
+	}, [history]);
 
-  return (
-    <div className="saved">
-      <h1 className="title">Saved Asteroids</h1>
-      <div className="cards">
-        {savedAsteroids &&
-          savedAsteroids.map((asteroid) => (
-            <SavedAsteroidCard
-              key={asteroid.id}
-              asteroid={asteroid}
-              className="card"
-              setAsteroidDetail={props.setAsteroidDetail}
-            />
-          ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="saved">
+			<h1 className="title">Saved Asteroids</h1>
+			<div className="savedCards">
+				{savedAsteroids &&
+					savedAsteroids.map((asteroid) => (
+						<SavedAsteroidCard
+							key={asteroid.id}
+							asteroid={asteroid}
+							className="card"
+							setAsteroidDetail={props.setAsteroidDetail}
+						/>
+					))}
+			</div>
+		</div>
+	);
 }
